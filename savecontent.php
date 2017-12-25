b66b3c5c2bc4072cea45a262fdaf3a8e8680a7f1
yotsawadi <34818767+yotsawadi@users.noreply.github.com>
2017-12-25 09:57:24 +0700

    ส

--- savecontent.php
+++ savecontent.php
@@ -16,9 +16,27 @@
 		$query = "INSERT INTO  tb_contect (numroom,name,phonenum,daytime,room,	floor,another,problem) VALUES ('$numroom','$name','$phonenum','$daytime','$room','$floor','$another','$problem');";
 		//echo $query;
 
-		$a= mysqli_query($dbcon, $query) or die(mysqli_error($dbcon));
+		 mysqli_query($dbcon, $query) or die(mysqli_error($dbcon));
 		
 		echo $a;
-		echo "บันทึกข้อมูลเรียบร้อย"
+		//echo "บันทึกข้อมูลเรียบร้อย"
 		
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>ระบบเเจ้งอุปกรณ์ชำรุดภายในหอพัก</title>
+	<link href="style.css" rel="stylesheet">
+	
+</head>
+<body>
+<div class="col-sm-8">
+	<h3>เมื่อเสร็จเเล้วจะเเจ้งให้ทราบ</h3>
+	<a href="index.php " class="btn btn-info btn-lg">
+          <span class="glyphicon glyphicon-log-out"></span> Log out
+        </a>
+       </div>
+
+</body>
+</html>
